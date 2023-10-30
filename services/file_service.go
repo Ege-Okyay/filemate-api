1f3425c6d9b4baf83df5d9b9f5bcde91bfc0328e
Ege-Okyay <83663983+Ege-Okyay@users.noreply.github.com>
2023-10-30 11:27:51 +0300

    Created services for more modular codebase

---
+++ services/file_service.go
@@ -0,0 +1,62 @@
+package services
+
+import (
+	"github.com/Ege-Okyay/filemate-api/models"
+	"github.com/Ege-Okyay/filemate-api/utils"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+func init() {
+	db = utils.GetDB()
+}
+
+func CreateFileRecords(fileRecords []models.File) error {
+	for _, record := range fileRecords {
+		result := db.Create(&record)
+		if result.Error != nil {
+			return result.Error
+		}
+	}
+
+	return nil
+}
+
+func FindFileByID(fileID string) (*models.File, error) {
+	var foundFile models.File
+	result := db.Where("id = ?", fileID).First(&foundFile)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &foundFile, nil
+}
+
+func DeleteFileRecord(file *models.File) error {
+	deleteResult := db.Delete(file)
+	if deleteResult.Error != nil {
+		return deleteResult.Error
+	}
+
+	return nil
+}
+
+func UpdateFileShareStatus(file *models.File, shared bool) error {
+	err := db.Model(file).Update("shared", shared)
+	if err.Error != nil {
+		return err.Error
+	}
+
+	return nil
+}
+
+func FindFilesByUserID(userID string) ([]models.File, error) {
+	var foundFiles []models.File
+	result := db.Where("user_id = ?", userID).Find(&foundFiles)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return foundFiles, nil
+}
